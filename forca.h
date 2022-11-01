72e3668081b87c545c2d559fd8442f87d04c4cc8
DanielL159 <danielluizdossantos12@gmail.com>
2022-11-01 15:19:27 -0300

    imcompleto

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define tamanho_palavra 20
+
+
+void abertura();
+void chuta();
+int jachutou (char letra );
+void desenhaForca();
+void escolhePalavra();
+int ganhou ();
+int enforcou();
+void adicionapalavra();
--- palavra_adivinhacao.c
+++ palavra_adivinhacao.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include<time.h>
+#include "forca.h"
 
-char palavrasecreta[20];
+
+char palavrasecreta[tamanho_palavra];
 char chutes[26];
-int tentativas =0;
+int chutesDados =0;
 
 void abertura(){
     printf("********************\n");
@@ -16,14 +20,14 @@ void chuta(){
  char chute ;
             scanf(" %c",&chute);
 
-            chutes[tentativas]=chute;
-        tentativas++;
+            chutes [chutesDados]=chute;
+     chutesDados++;
 }
 
 int jachutou (char letra ){
     int achou =0;
 
-    for (int j = 0; j < tentativas; j++)//verifica se as letras dentro da varivavel chute e igual a letra da palavr secreta
+    for (int j = 0; j < chutesDados; j++)//verifica se as letras dentro da varivavel chute e igual a letra da palavr secreta
         {
         if (chutes[j]== letra)
         {
@@ -35,10 +39,19 @@ int jachutou (char letra ){
 }
 
 void desenhaForca(){
+
+    printf("  _______      \n");
+    printf(" |/      |     \n");
+    printf(" |      (_)    \n");
+    printf(" |      \\|/   \n");
+    printf(" |       |     \n");
+    printf(" |      / \\   \n");
+    printf(" |             \n");
+    printf("_|___          \n");
+    printf("\n\n");
+
     for (int i =0 ; i < strlen(palavrasecreta);i++)//corre a palavra secreta
             {
-
-                
                 int achou = jachutou(palavrasecreta[i]);
                     
                 
@@ -53,20 +66,104 @@ void desenhaForca(){
             }
             printf("\n");
 }
+void adicionaPalavra(){
+    char quer ;
+    printf("DESEJA ADICIONAR UMA NOVA PALAVRA NO JOGO ? (S/N)");
+    scanf(" %c",&quer);
+
+    if (quer == 'S'){
+
+        char novapalavra[tamanho_palavra];
+        printf("Qual a nova palavra ?");
+        scanf(" %s",&novapalavra);
+
+        FILE*f;
+        
+        f= fopen("forca_palavras.txt","r+");
+
+        if (f==0){
+
+        printf("Perdao Banco de Dados nao Disponivel");
+        exit(1);
+        }
+
+        int qtd ;
+        fscanf(f,"%d",&qtd);
+        qtd++;
+
+        fseek(f,0,SEEK_SET);
+        fprintf(f,"%d",qtd);
+
+
+        fseek(f,0,SEEK_END);
+        fprintf(f, "\n%s",novapalavra);
+
+        fclose(f);
+    }
+}
 
 void escolhePalavra(){
+    FILE* f;
 
-    sprintf(palavrasecreta,"MELANCIA");
+    f= fopen("forca_palavras.txt","r");
+    if (f==0){
+        printf("Perdao Banco de Dados nao Disponivel");
+        exit(1);
+    }
+
+    int quantidadePalavras;
+    fscanf(f,"%d",quantidadePalavras);
+
+    srand(time(0));
+    int randomico = rand() % quantidadePalavras;
+
+    for (int i = 0; i < randomico; i++)
+    {
+       fscanf(f,"%s",palavrasecreta);
+    }
+    
+    fclose(f);
+}
+
+int ganhou (){
+
+    for (int i = 0; i < strlen(palavrasecreta); i++)
+    {
+        if (!jachutou(palavrasecreta[i]))
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
+
+int enforcou(){
+    int erros = 0;
+   
+
+    for (int i =0;i < chutesDados;i++){
+         int existe = 0 ,j;
+            for (  j = 0; j < strlen(palavrasecreta); j++)
+            {
+                if(chutes[i]== palavrasecreta[j]){
+                    existe = 1;
+                    break;
+                }
+            }
+
+         if (!existe)erros++; 
+
+    }
+
+    return erros >=5;
+    
 }
 
 
 
 int main (){
   
-
-    int acertou = 0;
-    int enforcou = 0;
-
     escolhePalavra();
   abertura();
 
@@ -74,11 +171,17 @@ int main (){
         {
             desenhaForca();
 
-          chuta();
+            chuta();
         
+       
+        } while (!ganhou()  && !enforcou() );
 
-        } while (!acertou  && !enforcou );
-    
+    if (ganhou()){
+        printf("Parabens vc acertou");
+    }else {
+        printf("Infelismente vc errou");
+    }
+   
 
 }
    
